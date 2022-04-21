1fc4c04de836df91c96772a0d843fd0611745b7a
umut9191 <umutsurmeli91@gmail.com>
2022-04-22 00:50:09 +0300

    On Order APi project; Address->Owned,Order,OrderItems,OrderStatus->enum created,EF packages downloaded ,Db connection configuration codes added wilt also AppDbContext and migration made

---
+++ Order.API/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.API.Models
+{
+    [Owned]//Address is not different table. It is Order's table.
+    public class Address
+    {
+        public string Line { get; set; }
+        public string Province { get; set; }
+        public string District  { get; set; }
+    }
+}
---
+++ Order.API/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.API.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        //we are adding Address model here because it will not has a db table, it will be in Order table 
+    }
+}
