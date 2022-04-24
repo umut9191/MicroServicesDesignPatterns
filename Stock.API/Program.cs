d3e749e942b583460092818311ff5a2653c63b71
umut9191 <umutsurmeli91@gmail.com>
2022-04-24 23:34:59 +0300

    EF core ,EF core in memory packages added to StockApi layer, Context ,db set and seed(initialize stockDb Stocks table values ) codes added

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("StockDb");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +27,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Stocks.Add(new Stock.API.Models.Stock() { Id = 1, ProductId=1, Count=100 });
+    context.Stocks.Add(new Stock.API.Models.Stock() { Id = 2, ProductId = 2, Count = 100 });
+    context.SaveChanges();
+}
 app.Run();
