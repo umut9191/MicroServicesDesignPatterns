41ef4d3352249c757f96e43fe8bcbd3324cccc3a
umut9191 <umutsurmeli91@gmail.com>
2022-04-25 22:27:25 +0300

    stock controller created

---
+++ Stock.API/Controllers/StocksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Models;
+
+namespace Stock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        //for just to see what is in stock
+        //readonly is for just can be set on constructer
+        private readonly AppDbContext _context;
+
+        public StocksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Stocks.ToListAsync());
+        }
+    }
+}
