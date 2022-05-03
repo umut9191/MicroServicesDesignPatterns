cfbd406b2300188a892866cc2b01e22594866021
umut9191 <umutsurmeli91@gmail.com>
2022-05-03 16:44:42 +0300

    OrderStateMachine configuretion settings codes added

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,9 +1,41 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.Models;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Shared;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+
+    .ConfigureServices((ctx, services) =>
     {
-        services.AddHostedService<Worker>();
+    services.AddHostedService<Worker>();
+
+    services.AddMassTransit(cfg => {
+
+    cfg.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>
+        ().EntityFrameworkRepository(opt =>
+        {
+            opt.AddDbContext<DbContext, OrderStateDbContext>((provider, builder) =>
+            {
+                builder.UseSqlServer(ctx.Configuration.GetConnectionString("SqlCon"), m =>
+                {
+                    m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                });
+            });
+        });
+
+    cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>{
+        configure.Host(ctx.Configuration.GetConnectionString("RabbitMQAMQPURL"));
+        configure.ReceiveEndpoint(RabbitMqSettingsConsts.OrderSaga, e => {
+            e.ConfigureSaga<OrderStateInstance>(provider);
+        
+        });
+    
+    }));
+
+        });
     })
     .Build();
 
