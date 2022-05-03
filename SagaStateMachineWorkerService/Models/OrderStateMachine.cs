8b5085fbc1b5813d1f688a2cda301b796e4d340e
umut9191 <umutsurmeli91@gmail.com>
2022-05-03 20:19:01 +0300

    OrderStateMachine codes added

--- SagaStateMachineWorkerService/Models/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Models/OrderStateMachine.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,34 @@ namespace SagaStateMachineWorkerService.Models
 {
     public class OrderStateMachine:MassTransitStateMachine<OrderStateInstance>
     {
+        public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }
+        public State OrderCreated { get; private set; }
+
+        public OrderStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+            //aynısı db de var ise tekrar oluşmasın;
+            //eğer yok ise yeni bir satır oluştur ve oluşturduğun OrderStateInstance in CorrelationId ne yeni bir değer ata.
+            Event(() => OrderCreatedRequestEvent, y => y.CorrelateBy<int>(x => x.OrderId, z => z.Message.OrderId)
+            .SelectId(context=>Guid.NewGuid()));
+            //instanceler veritabanındaki Data lar ise Eventteki 
+            Initially(When(OrderCreatedRequestEvent).Then(context =>
+            {
+                context.Instance.BuyerId = context.Data.BuyerId;
+                context.Instance.OrderId = context.Data.OrderId;
+                context.Instance.CreatedDate = DateTime.Now;
+                context.Instance.CardName = context.Data.Payment.CardName;
+                context.Instance.CardNumber = context.Data.Payment.CardNumber;
+                context.Instance.CVV = context.Data.Payment.CVV;
+                context.Instance.Expiration = context.Data.Payment.Expiration;
+                context.Instance.TotalPrice = context.Data.Payment.TotalPrice;
+
+            }).Then(context => { Console.WriteLine($"OrderCreatedRequestEvent before : {context.Instance}");//toString() override methodu çalışacak.
+            }).TransitionTo(OrderCreated)//initialdan order created e geçilsin
+            .Then(context => {
+                Console.WriteLine($"OrderCreatedRequestEvent after : {context.Instance}");//toString() override methodu çalışacak.
+            })
+            );
+        }
     }
 }
