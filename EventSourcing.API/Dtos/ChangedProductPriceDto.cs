a55a9d4ae659e29390a0e652db8eb80ba13d6f6a
umut9191 <umutsurmeli91@gmail.com>
2022-05-16 22:29:23 +0300

    Dtos classes created with new Asp.Net Core app

---
+++ EventSourcing.API/Dtos/ChangedProductPriceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EventSourcing.API.Dtos
+{
+    public class ChangedProductPriceDto
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ EventSourcing.API/Dtos/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EventSourcing.API.Dtos
+{
+    public class CreateProductDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
