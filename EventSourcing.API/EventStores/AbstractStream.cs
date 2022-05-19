8fd6680b312e9d0c0d82238c7dc9968615445403
umut9191 <umutsurmeli91@gmail.com>
2022-05-19 12:14:18 +0300

    EventStore.Client package installed, AbstractStream class created

---
+++ EventSourcing.API/EventStores/AbstractStream.cs
@@ -0,0 +1,37 @@
+﻿using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+using System.Text;
+using System.Text.Json;
+
+namespace EventSourcing.API.EventStores
+{
+    public abstract class AbstractStream
+    {
+        protected readonly LinkedList<IEvent> Events = new LinkedList<IEvent>();
+        private string _streamName { get;  }
+
+        private readonly IEventStoreConnection _eventStoreConnection;
+
+        protected AbstractStream(string streamName, IEventStoreConnection eventStoreConnection)
+        {
+            _streamName = streamName;
+            _eventStoreConnection = eventStoreConnection;
+        }
+        //event store a event kaydedebilmemiz için mutlaka EventData oluşturmalıyız o dayine EventStore.Client paketinten geliyor.
+        public async Task SaveAsync()
+        {
+            //buraya gelen eventler bu şekilde bir event dataya dönüştürülüyor.
+            var newEvents = Events.ToList().Select(x =>
+            new EventData(
+                Guid.NewGuid(),
+                x.GetType().Name,
+                true,
+                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(x,inputType:x.GetType())),
+                Encoding.UTF8.GetBytes(x.GetType().FullName)
+                )).ToList();
+
+            await _eventStoreConnection.AppendToStreamAsync(_streamName, ExpectedVersion.Any,newEvents);
+            Events.Clear();
+        }
+    }
+}
